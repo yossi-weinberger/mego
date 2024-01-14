eb271ced180492c8ffd98742f76b4c40c5552348
Yossi Weinberger <137178593+yossi-weinberger@users.noreply.github.com>
2024-01-14 02:37:51 +0200

    game

--- full stack/projects/Java script game/game.js
+++ full stack/projects/Java script game/game.js
@@ -3,18 +3,27 @@
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
 
+
+
 // constant movment by direction
+
 function game_play() {
 
-  console.log(enemies)
-  document.getElementById("score").innerHTML = score;
+  //plays aoudio
+window.onload = function () {
+  song.play();
+};
 
-  console.log(enemies)
+document.body.onkeydown = function () {
+  song.play();
+};
+
+  document.getElementById("score").innerHTML = score;
   document.getElementById("lives").innerHTML = lives;
 
   //cleans canvas
   ctx.drawImage(moon_img, 0, 0, 500, 500);
- 
+
   // draws apple player enemys and walls
   apple();
   player();
@@ -24,21 +33,15 @@ function game_play() {
 
 //----------------------------------------------------
 function key_strike() {
-  // putting new valeus of player location
+  // putting new valeus of player diraction
   if (event.keyCode == 40 && y < 480 && y >= 0) direction = "d";
   if (event.keyCode == 38 && y <= 480 && y > 0) direction = "u";
   if (event.keyCode == 37 && x <= 480 && x > 0) direction = "l";
   if (event.keyCode == 39 && x < 480 && x >= 0) direction = "r";
 }
 
-function apple(){
-  
-  // ctx.drawImage(apple_img, apple_x, apple_y, block_size, block_size);
-  
-  for (let i = 0; i < apples.length; i++) {
-    let apple = apples[i];
-    ctx.drawImage(apple_img, apple.apple_x, apple.apple_y, block_size, block_size);
-  }
+function apple() {
+  ctx.drawImage(apple_img, apple_x, apple_y, block_size, block_size);
 }
 
 function player() {
@@ -57,38 +60,33 @@ function player() {
   //if player meets enemys
   for (let i = 0; i < enemies.length; i++) {
     let enemy = enemies[i];
-    if (x == enemy.enemy_x && y == enemy.enemy_y){
-    window.alert("You lost 1 live!");
-    lives -= 1}
-    if (direction == "l" && x +20 == enemy.enemy_x && y == enemy.enemy_y){
+    if (x == enemy.enemy_x && y == enemy.enemy_y) {
+      hit_sound.play();
       window.alert("You lost 1 live!");
-    lives -= 1;}
+      lives -= 1;
+    }
+    if (direction == "l" && x + 20 == enemy.enemy_x && y == enemy.enemy_y) {
+      hit_sound.play();
+      window.alert("You lost 1 live!");
+      lives -= 1;
+    }
   }
-  //game over if hit by enemy 3 times
+//resat if game over
   if (lives == 0) {
-    window.alert("game over!!!\n")
+    window.alert("game over!!!\n");
     score = 0;
     lives = 3;
   }
 
   //if player meets apple
-  for (let i = 0; i < apples.length; i++) {
-    let apple = apples[i];
-    if (x == apple.apple_x && y == apple.apple_y){
-    // apple();
+  if (x == apple_x && y == apple_y) {
+    //place the apple in a new random location
+    bite_sound.play();
+    xr = Math.floor(Math.random() * 460);
+    yr = Math.floor(Math.random() * 460);
+    apple_x = xr + (20 - (xr % 20));
+    apple_y = yr + (20 - (yr % 20));
     score += 1;
-    apple_random();
-    apple()
-  }
-
-
-  // if (x == apple_x && y == apple_y) {
-  //   //place the apple in a new random location
-  //   xr = Math.floor(Math.random() * 460);
-  //   yr = Math.floor(Math.random() * 460);
-  //   apple_x = xr + (20 - (xr % 20));
-  //   apple_y = yr + (20 - (yr % 20));
-  //   score += 1;
   }
 
   //if player meets wall
@@ -122,7 +120,7 @@ function player() {
     //set the privios direction
     previos_direction = direction;
 
-    //draw new player and apple
+    //draw player in curent direction
     if (direction == "r") ctx.drawImage(player_r, x, y, block_size, block_size);
     else if (direction == "l")
       ctx.drawImage(player_l, x, y, block_size, block_size);
@@ -135,12 +133,11 @@ function player() {
   }
 }
 
-
-function enemys() {  
+function enemys() {
   // draw enemys
   for (let i = 0; i < enemies.length; i++) {
     let enemy = enemies[i];
-    ctx.drawImage(enemy_img, enemy.enemy_x, enemy.enemy_y, block_size, block_size);
+    ctx.drawImage(enemy_img, enemy.enemy_x,enemy.enemy_y,block_size,block_size);
   }
 
   //enemies poping from the other side
@@ -149,36 +146,41 @@ function enemys() {
     if (enemy.enemy_y >= 500) enemy.enemy_y = 0;
     if (enemy.enemy_x >= 500) enemy.enemy_x = 0;
   }
-  
-    // movment of enemys
-    for (let i = 0; i < enemies.length; i++) {
-      let enemy = enemies[i];
-    if (
-      (direction == "d" && enemy.enemy_y != y) || (direction == "u" && enemy.enemy_y != y))
-      enemy.enemy_y += block_size;
-    if (
-      (direction == "d" && enemy.enemy_y == y) || (direction == "u" && enemy.enemy_y == y))
-      enemy.enemy_x += block_size;
-    if (
-      (direction == "l" && enemy.enemy_y != y) || (direction == "r" && enemy.enemy_y != y))
-      enemy.enemy_y += block_size;
-    if (
-      (direction == "l" && enemy.enemy_y == y) || (direction == "r" && enemy.enemy_y == y))
-      enemy.enemy_x += block_size;
-    if (direction == "stop" ) enemy.enemy_x += block_size;
-    }
-}
 
+  // movment of enemys
+  for (let i = 0; i < enemies.length; i++) {
+    let enemy = enemies[i];
+    if (
+      (direction == "d" && enemy.enemy_y != y) ||
+      (direction == "u" && enemy.enemy_y != y)
+    )
+      enemy.enemy_y += block_size;
+    if (
+      (direction == "d" && enemy.enemy_y == y) ||
+      (direction == "u" && enemy.enemy_y == y)
+    )
+      enemy.enemy_x += block_size;
+    if (
+      (direction == "l" && enemy.enemy_y != y) ||
+      (direction == "r" && enemy.enemy_y != y)
+    )
+      enemy.enemy_y += block_size;
+    if (
+      (direction == "l" && enemy.enemy_y == y) ||
+      (direction == "r" && enemy.enemy_y == y)
+    )
+      enemy.enemy_x += block_size;
+    if (direction == "stop") enemy.enemy_x += block_size;
+  }
+}
 
 function drow_walls() {
-    for (var i = 0; i < walls.length; i++)
-  ctx.drawImage(brick_img, walls[i].x, walls[i].y, block_size, block_size); 
+  for (var i = 0; i < walls.length; i++)
+    ctx.drawImage(brick_img, walls[i].x, walls[i].y, block_size, block_size);
 
-    for (var i = 0; i < portals.length; i++)
-    ctx.drawImage(portal_img, portals[i].x, portals[i].y, block_size, block_size); 
+  for (var i = 0; i < portals.length; i++)
+    ctx.drawImage(portal_img,portals[i].x,portals[i].y,block_size,block_size);
 }
-
-
 
 //creating screen by array
 var screen = Array(
@@ -208,6 +210,7 @@ var screen = Array(
   1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
   1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
 
+
 walls = new Array();
 portals = new Array();
 
@@ -218,11 +221,11 @@ screen_y = 0;
 for (var i = 0; i < screen.length; i++) {
   screen_x = i % 25;
   screen_y = Math.floor(i / 25);
-  
+
   if (screen[i] == 1) {
-    walls.push({ x:screen_x * 20, y: screen_y * 20 });
+    walls.push({ x: screen_x * 20, y: screen_y * 20 });
   } else if (screen[i] == 2) {
-    portals.push({ x:screen_x * 20, y: screen_y * 20 });
+    portals.push({ x: screen_x * 20, y: screen_y * 20 });
   }
 }
 
@@ -264,38 +267,25 @@ apple_img.src = "pics/seed.webp";
 var portal_img = new Image();
 portal_img.src = "portal.png";
 
+//sounds
+let song = new Audio();
+song.src = "sound/background.ogg";
+song.loop = true;
 
-// creating random location for apple and enemies
-function random_num(){
-  return Math.floor(Math.random() * 25) * 20;
+var hit_sound = new Audio();
+hit_sound.src = "sound/hit.wav";
+
+var bite_sound = new Audio();
+bite_sound.src = "sound/bite.wav";
+
+// creating random num for apple and enemies
+function random_num() {
+  return Math.floor(Math.random() * 24) * 20 + 20;
 }
-
 
 //putts apple in random location
-var xr = Math.floor(Math.random() * 480);
-var yr = Math.floor(Math.random() * 480);
-var apple_x = xr + (20 - (xr % 20));
-var apple_y = yr + (20 - (yr % 20));
-
-
-
-// apples
-function apple_random(){
-  apples = new Array();
-
-  apple_1 = {
-    apple_x: random_num() + (20 - (random_num() % 20)),
-    apple_y: random_num() + (20 - (random_num() % 20)),
-  };
-
-  apple_2 = {
-    apple_x: random_num() + (20 - (random_num() % 20)),
-    apple_y: random_num() + (20 - (random_num() % 20)),
-  };
-
-  apples.push(apple_1, apple_2);
-
-}
+var apple_x = random_num() + (20 - (random_num() % 20));
+var apple_y = random_num() + (20 - (random_num() % 20));
 
 // enemys
 enemies = new Array();
@@ -312,17 +302,14 @@ enemy_4 = {
 
 enemies.push(enemy_3, enemy_4);
 
-
 var previos_direction = 0;
 var score = 0;
 var lives = 3;
 var block_size = 20;
 var direction = "r";
 
-
 //calls script
 document.onkeydown = key_strike;
 // constent movment
-apple_random()
-time = 200;
+time = 180;
 setInterval(game_play, time);
